203bf6bd6a48790ce7fa6e0b4ca706b9009dbbbe
Karanthakurt <90297026+Karanthakurt@users.noreply.github.com>
2022-04-09 18:26:41 +0530

    Add files via upload

---
+++ Assignment1/src/com/karan/Program1.java
@@ -0,0 +1,15 @@
+package com.karan;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		karan kt = new karan();
+		kt.show();
+		
+	}
+
+}
---
+++ Assignment1/src/com/karan/karan.java
@@ -0,0 +1,9 @@
+package com.karan;
+
+
+public class karan {
+
+	 void show() {
+		 System.out.println("This is show method");
+	 }
+}
